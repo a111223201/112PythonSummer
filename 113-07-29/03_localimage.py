920d4044251add57cac1fe2fcb7c1770668cd45e
a111223201 <a111223201@mail.shu.edu.tw>
2024-07-29 15:54:51 +0800

    113-07-29

--- 113-07-29/03_localimage.py
+++ 113-07-29/03_localimage.py
@@ -4,6 +4,6 @@ URL = 'https://notify-api.line.me/api/notify'
 H, P, F= {}, {}, {}
 H['Authorization'] = 'Bearer Jtd5DlFVD4kYq58F0m0ycM44EQmv00GDSfLRjs1QH5H'
 P['message'] = '本機圖片'
-F['imageFile'] = open('name.jpg','rb')
-requests.post(URL, headers=H, params=P)
+F['imageFile'] = open('name.png','rb')
+requests.post(URL, headers=H, params=P,files=F)
 
